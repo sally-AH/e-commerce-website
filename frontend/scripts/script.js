915bb7e7906299cb4f59bffb95fd8f52b41a7ee8
sally-AH <sallyabdalhafeez@gmail.com>
2023-07-31 11:41:10 +0300

    <frontend> update script file

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,9 +1,12 @@
-const singin_btn = document.getElementById("btn_transparent");
+const singin_btn = document.getElementById("login");
 const signup_btn = document.getElementById("btn_signup");
 const signin_back = document.getElementById("back_signin");
 
+
 singin_btn.addEventListener("click", function() {
     document.getElementById('id01').style.display='block';
+    document.getElementById('heart').style.display='block';
+    document.getElementById('profile').style.display='block';
 });
 
 signup_btn.addEventListener("click", function(){
@@ -15,4 +18,5 @@ signin_back.addEventListener("click", function(){
     document.getElementById('id01').style.display='block';
     document.getElementById('id02').style.display='none';
 });
+
 
